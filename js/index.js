537d0841be1523f4aa22d598d2925bbd32895c0e
15290926901 <74633638+15290926901@users.noreply.github.com>
2021-08-03 14:59:58 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,27 @@
+// X 
+
+var tr = document.getElementById("ture");
+var fa = document.getElementById("false");
+var nav = document.getElementById("content");
+var num = 0;
+// nav.style.opacity = "0";
+function show(i) {
+	
+	num++;
+	if (num % 2 == 0) {
+		tr.style.display = "block";
+		fa.style.display = "none";
+
+		nav.style.left="-100%";
+		// nav.style.opacity = "0"
+	} else {
+		tr.style.display = "none";
+		fa.style.display = "block";
+		// nav.style.display = "block";
+		
+		nav.style.left="0";
+		
+		nav.style.transition = ".5s";
+	}
+}
+
